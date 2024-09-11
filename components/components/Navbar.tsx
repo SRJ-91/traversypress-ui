71a40c0dcc12a84e8f8b81f45b80f0782b5b43bb
SRJ-91 <Sawyerjen@hotmail.com>
2024-09-11 12:16:36 -0500

    avatar fallback and links

--- components/components/Navbar.tsx
+++ components/components/Navbar.tsx
@@ -2,21 +2,45 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "../../img/logo.png"
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/components/ui/avatar'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/components/ui/dropdown-menu"
 
 
 
 
 const Navbar = () => {
-    return ( <div className="bg-primary dark:bg-slate-700 text-white text-white py-2 px-5 flex justify-between">
+    return (<div className="bg-primary dark:bg-slate-700 text-white text-white py-2 px-5 flex justify-between">
         <Link href='/'>
-            <Image src={ logo } alt="TraversyPress" width={40} />
+            <Image src={logo} alt="TraversyPress" width={40} />
         </Link>
 
-        <Avatar>
-                <AvatarImage src='https://github.com/shadcn.png' alt='at@shadcn' />
-                <AvatarFallback className='text-black'>BT</AvatarFallback>
-            </Avatar>
-    </div> );
+
+
+        <DropdownMenu>
+            <DropdownMenuTrigger className="focus:outliine-none">
+                <Avatar>
+                    <AvatarImage src='https://github.com/shadcn.png' alt='at@shadcn' />
+                    <AvatarFallback className='text-black'>BT</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                    <Link href={'/profile'}>Profile</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Link href={'/auth'}>Logout</Link>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    </div>);
 }
- 
+
 export default Navbar;
